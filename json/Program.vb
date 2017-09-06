c61c24fcbc3890b3a383419c46dde83c5d535cfd
xieguigang <xie.guigang@gcmodeller.org>
2017-09-06 18:55:05 +0800

    apply map filter

--- json/Program.vb
+++ json/Program.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::6189bf440bd5fce4a59c929a119c2931, ..\KEGG_canvas\json\Program.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -32,6 +32,7 @@ Imports System.IO
 Imports json.csv
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.Ranges
 Imports Microsoft.VisualBasic.Data.csv
@@ -54,7 +55,19 @@ Public Module Program
         Return GetType(Program).RunCLI(App.CommandLine)
     End Function
 
-    Public Function Convert(in$, nodesTable$, kegKCF$, degreeSize As Boolean, compress As Boolean, style$, nodeID As Boolean) As String
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="in$"></param>
+    ''' <param name="nodesTable$"></param>
+    ''' <param name="kegKCF$"></param>
+    ''' <param name="degreeSize"></param>
+    ''' <param name="compress"></param>
+    ''' <param name="style$"></param>
+    ''' <param name="nodeID"></param>
+    ''' <param name="maps$">如果指定了这个参数，则只会输出这个数组之中指定的途径的分组信息</param>
+    ''' <returns></returns>
+    Public Function Convert(in$, nodesTable$, kegKCF$, degreeSize As Boolean, compress As Boolean, style$, nodeID As Boolean, Optional maps$() = Nothing) As String
         Dim data = [in].LoadCsv(Of network_Csv)
         Dim nodeDatas = nodesTable _
             .LoadCsv(Of nodeData) _
@@ -188,33 +201,60 @@ Public Module Program
                 .Data = x.Data
             }
 
-        Dim groupColors As Color() = {
+        Dim groupColors As Color()
+        Dim groupNames As Dictionary(Of String, String)
+
+        If maps.IsNullOrEmpty Then
+            groupColors = {
             Color.SteelBlue, Color.SkyBlue, Color.Teal, Color.DarkMagenta, Color.PaleTurquoise, Color.PaleGreen, Color.Crimson, Color.DodgerBlue
         }
-        ' = Designer.GetColors("d3.scale.category10()") _
-        '    .Take(8) _
-        '    .ToArray
-        Dim pathways = nodes _
-            .Where(Function(x) Not x.type.StringEmpty) _
-            .Select(Function(x) x.type.Split("|"c)) _
-            .IteratesALL _
-            .GroupBy(Function(x) x.Split.First) _
-            .Where(Function(g) g.Count > 3) _
-            .OrderByDescending(
-                Function(g)
-                    Dim nid = g.First
+            ' = Designer.GetColors("d3.scale.category10()") _
+            '    .Take(8) _
+            '    .ToArray
 
-                    Return Aggregate node As node
-                           In nodeTable _
-                               .Values _
-                               .Where(Function(n) InStr(n.type, nid, CompareMethod.Text) > 0)
-                           Into Average(node.degree)
-                End Function) _
-            .Take(groupColors.Length) _
-            .ToArray
-        Dim groupNames = pathways _
-            .ToDictionary(Function(x) x.Key,
-                          Function(name) name.First)
+            Dim pathways = nodes _
+                .Where(Function(x) Not x.type.StringEmpty) _
+                .Select(Function(x) x.type.Split("|"c)) _
+                .IteratesALL _
+                .GroupBy(Function(x) x.Split.First) _
+                .Where(Function(g) g.Count > 3) _
+                .OrderByDescending(
+                    Function(g)
+                        Dim nid = g.First
+
+                        Return Aggregate node As node
+                                   In nodeTable _
+                                   .Values _
+                                   .Where(Function(n) InStr(n.type, nid, CompareMethod.Text) > 0)
+                               Into Average(node.degree)
+                    End Function) _
+                .Take(groupColors.Length) _
+                .ToArray
+
+            groupNames = pathways _
+                .ToDictionary(Function(x) x.Key,
+                              Function(name) name.First)
+        Else
+            Dim mapIndex As Index(Of String) = maps.Indexing
+            Dim pathways = nodes _
+              .Where(Function(x) Not x.type.StringEmpty) _
+              .Select(Function(x) x.type.Split("|"c)) _
+              .IteratesALL _
+              .GroupBy(Function(x) x.Split.First) _
+              .Where(Function(g) mapIndex(g.Key.Trim("["c, "]"c).Trim) > -1) _
+              .ToArray
+
+            groupNames = pathways _
+                .ToDictionary(Function(x) x.Key,
+                              Function(name) name.First)
+
+            If groupNames.Count > 30 Then
+                groupColors = Designer.GetColors("scibasic.category31()", groupNames.Count + 1)
+            Else
+                groupColors = Designer.GetColors("scibasic.category31()")
+            End If
+
+        End If
 
         Call groupNames.GetJson.__INFO_ECHO
 
