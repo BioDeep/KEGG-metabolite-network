dd7311e0e03562d7c09bf43abdc6bc51047685fc
xieguigang <xie.guigang@gcmodeller.org>
2018-01-11 12:44:46 +0800

    add tree test

---
+++ json/test.vb
@@ -0,0 +1,12 @@
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+
+Module test
+
+    Sub Main()
+        Dim c As Compound = "D:\smartnucl_integrative\DATA\KEGG\KEGG_cpd\OtherUnknowns\C00001.xml".LoadXml(Of Compound)
+
+        Call KCF.CreateTable("D:\smartnucl_integrative\DATA\KEGG\KEGG_cpd\Compounds with biological roles\Vitamins and Cofactors\Vitamins\Water-soluble vitamins [Fig]")
+
+        Pause()
+    End Sub
+End Module
