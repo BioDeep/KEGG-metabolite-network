6721814db76274db1b2e207c4ad253046543abea
xieguigang <xie.guigang@gcmodeller.org>
2018-10-30 15:47:16 +0800

    add web http interface

--- WebServices/App.vb
+++ WebServices/App.vb
@@ -1,14 +1,28 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.WebCloud.HTTPInternal.AppEngine
 Imports SMRUCC.WebCloud.HTTPInternal.AppEngine.APIMethods
 Imports SMRUCC.WebCloud.HTTPInternal.AppEngine.APIMethods.Arguments
 Imports SMRUCC.WebCloud.HTTPInternal.Platform
 
-<[Namespace]("app")> Public Class App : Inherits WebApp
+''' <summary>
+''' 网络模型渲染任务后台
+''' </summary>
+<[Namespace]("KEGG_canvas")> Public Class AppCanvas : Inherits WebApp
+
+    ReadOnly appPath As String
 
     Public Sub New(main As PlatformEngine)
         MyBase.New(main)
+
+        appPath = App.GetVariable("render") Or $"{App.HOME}/render/KEGG_canvas.exe".AsDefault
+
+        If appPath.FileExists Then
+            Call $"Render Engine found at: {appPath}".__DEBUG_ECHO
+        Else
+            Call $"Render Api will not working unless you have put ``KEGG_canvas`` CLI to location: {appPath}".Warning
+        End If
     End Sub
 
     ''' <summary>
@@ -18,10 +32,26 @@ Imports SMRUCC.WebCloud.HTTPInternal.Platform
     ''' <param name="request"></param>
     ''' <param name="response"></param>
     ''' <returns></returns>
-    <ExportAPI("/app/render.vbs")>
+    <ExportAPI("/KEGG_canvas/render.vbs")>
     <POST(GetType(String()))>
     Public Function Render(request As HttpPOSTRequest, response As HttpResponse) As Boolean
         Dim kegg$() = request.POSTData("kegg").LoadJSON(Of String())
+        Dim tempfile = App.GetAppSysTempFile(".txt", App.PID)
+        Dim outfile = App.GetAppSysTempFile(".json", App.PID)
 
+        Call kegg.GetJson.__INFO_ECHO
+
+        With New Program.KEGG_canvas(appPath)
+            If Not .IsAvailable Then
+                ' do nothing
+                Call "Render Engine is Missing, could not process user request!".Warning
+            Else
+                Call kegg.FlushAllLines(tempfile)
+                Call .ReconstructKEGGNetwork(tempfile, outfile)
+                Call response.Write(outfile.ReadAllText)
+            End If
+        End With
+
+        Return True
     End Function
 End Class
