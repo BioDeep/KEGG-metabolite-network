1eb5f69cb12e41da5af35040251baa1896e5b1bb
xieguigang <xie.guigang@gcmodeller.org>
2018-12-23 14:42:45 +0800

    update submodule

--- typescript/KEGG/app.ts
+++ typescript/KEGG/app.ts
@@ -1,6 +1,7 @@
 ﻿namespace KEGGBrite {
 
     export function parse(briteText: string) {
+        var tree: IKEGGBrite = JSON.parse(briteText);
 
     }
 
@@ -10,6 +11,10 @@
         var names: string[] = $ts(list).Skip(1).Select(s => s.split(/;\s*/g)).Unlist(x => x).ToArray();
 
         return { id: id, names: names };
+    }
+
+    function isLeaf(node: IKEGGBrite): boolean {
+        return IsNullOrEmpty(node);
     }
 }
 
@@ -22,3 +27,5 @@ interface IDEntry {
     id: string;
     names: string[];
 }
+
+interface 
