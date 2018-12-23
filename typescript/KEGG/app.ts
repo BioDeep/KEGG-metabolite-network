b541e8d5e4b763b2e0fcce3b57601d18548bc312
xieguigang <xie.guigang@gcmodeller.org>
2018-12-23 14:37:01 +0800

    parse id entry

--- typescript/KEGG/app.ts
+++ typescript/KEGG/app.ts
@@ -1,1 +1,24 @@
-﻿console.log('Hello world');
+﻿namespace KEGGBrite {
+
+    export function parse(briteText: string) {
+
+    }
+
+    function parseIDEntry(text: string): IDEntry {
+        var list = text.split(/\s{2,}/g);
+        var id: string = list[0];
+        var names: string[] = $ts(list).Skip(1).Select(s => s.split(/;\s*/g)).Unlist(x => x).ToArray();
+
+        return { id: id, names: names };
+    }
+}
+
+interface IKEGGBrite {
+    name: string;
+    children: IKEGGBrite[];
+}
+
+interface IDEntry {
+    id: string;
+    names: string[];
+}
